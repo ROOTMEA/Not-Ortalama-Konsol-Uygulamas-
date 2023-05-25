25ee0d3af8a6f3e67111e8e3d71072125e0e76a1
Efe Aygün <82315578+ROOTMEA@users.noreply.github.com>
2023-05-25 15:19:33 +0300

    Add files via upload

---
+++ Not-Ortalama-Konsol-Uygulaması/ConsoleApp1(NOT-ORTALAMA-HESAPLAMA)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_NOT_ORTALAMA_HESAPLAMA_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int not1,not2,ort;
+
+            Console.WriteLine("I.Notunuzu Girin:");
+            not1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("II.Notunuzu Girin:");
+            not2=Convert.ToInt32(Console.ReadLine());
+
+            ort = (not1+not2)/2;
+
+            //Geçti Kaldı işlemi
+
+            if (ort < 50)
+            {
+                Console.WriteLine("Kaldı");
+            }
+            else if(ort >=50 && ort <=100)
+            {
+                Console.WriteLine("Geçti");
+            }
+
+            Console.WriteLine("Ortalama:"+ort);
+
+            Console.WriteLine("Çıkmak için bir tuşa basınız!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Not-Ortalama-Konsol-Uygulaması/ConsoleApp1(NOT-ORTALAMA-HESAPLAMA)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ConsoleApp1(NOT-ORTALAMA-HESAPLAMA)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp1(NOT-ORTALAMA-HESAPLAMA)")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("f2a62dbf-401c-471e-9a2a-9457c8df45f9")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
